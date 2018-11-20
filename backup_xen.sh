aacd1e503e44b23ac20bc21d6c58e31173a7855d
Jorge Luiz Taioque <jorgeluiztaioque@gmail.com>
2018-11-19 22:50:39 -0200

    add remote nfs server

--- backup_xen.sh
+++ backup_xen.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-#Destino onde ficara guardado o backup
-dir="/mnt/usb/";
+# Connecting to remote NFS storage
+localdir="/mnt/nasstorage";
+nfsserver="200.200.200.200"
+nfsremotedir="/home/backupxen/vms"
 
-#Pegando nomes das maquinas
+# mount remove nfs server
+mount $nfsserver:$nfsremotedir $localdir
+
+# Geting VM informations
 uuids=$(xe vm-list is-control-domain=false is-a-snapshot=false power-state=running | grep "uuid" | awk '{ print $'5'}');
 labels=$(xe vm-list is-control-domain=false is-a-snapshot=false power-state=running | grep "name-label" | awk '{ print $'4' $'5' $'6' $'7' $'8' $'9' $'10'}');
 qvms=$(echo "$uuids" | wc -l);
 
-#Loop para backup de cada VM
-for i in `seq 1 $qvms`
-do
-uuid=$(echo $uuids | awk '{ print $'$i'}');
-label=$(echo $labels | awk '{ print $'$i'}');
+#Loop to get id and vm labels
+for i in `seq 1 $qvms`; do
+  uuid=$(echo $uuids | awk '{ print $'$i'}');
+  label=$(echo $labels | awk '{ print $'$i'}');
 
-# creating a snapshot
-snuuid=$(xe vm-snapshot uuid=$uuid new-name-label=$label-backup)
+  # creating a snapshot
+  snuuid=$(xe vm-snapshot uuid=$uuid new-name-label=$label-backup)
 
-# Turn snapshot in a template
-xe template-param-set is-a-template=false ha-always-run=false uuid=$snuuid
+  # Turn snapshot in a template
+  xe template-param-set is-a-template=false ha-always-run=false uuid=$snuuid
 
-# export template as a backup to remote server
-xe vm-export vm=$snuuid filename=$dir$label-backup.xva
+  # export template as a backup to remote server
+  xe vm-export vm=$snuuid filename=$dir$label-backup.xva
 
-#remove a template
-xe vm-uninstall uuid=$snuuid force=true
+  #remove a template
+  xe vm-uninstall uuid=$snuuid force=true
+
+  sleep 10
 
 done
